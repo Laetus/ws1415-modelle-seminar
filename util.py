920ac20db783c08da76ba631022950c7b49e03a0
Philipp Fröhlich <philipp-froehlich@hotmail.com>
2014-11-10 19:24:05 +0100

    neuerPunkt und neueRichtung Methoden gibt es jetzt auch für die Aufpunkt + (lambda * Richtung) Repräsentation

--- util.py
+++ util.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as pp
 
 """ Allgemeiner Plan
 
-Darstellung von Vektoren immer als v =  np.array([x,y]) oder np.array([x,y,z]) im dreidimensionalen. Denn Operationen mit v sind dann besser lesbar. Dies verbessert die Lesbarkeit denn v * 2  ist dann z.B. np.array([2x , 2y]) und nicht (x , y, x,y )
+Darstellung von Vektoren immer als v =  np.array([x,y]) oder np.array([x,y,z]) im dreidimensionalen. Denn Operationen mit v sind dann besser lesbar. Dies verbessert die Lesbarkeit denn v * 2  ist dann z.B. np.array((2x , 2y)) und nicht (x , y, x,y )
 
 if(isOnEllipse(a, b, s)):
 	def Billiard(s, steigung, a,b):
@@ -21,6 +21,30 @@ benachbarte punkte bleiben benachbart a b y ( immer a neben b)
 """
 
 
+def startBilliard(x0,d0, it):
+	x0 = x0 * 1.0
+	d0 = d0 * 1.0
+	
+	listX = np.array( [x0] )	
+	listD = np.array( [d0] )
+
+	for x in range (0 ,it) :
+		'Berechne neuen Punkt'
+		x_neu = neuerPunktV(x0,d0)
+		'Berechne neue Richtung'
+		d_neu = berechneNeueRichtung(x_neu, d0)
+
+		'Schreibe neune Punkte und Richtungen weg'
+		listX = np.append(listX, [x_neu], axis=0)
+		listD = np.append(listD, [d_neu], acis=0)
+		
+		'Bereite neue Iteration vor'
+		x0 = x_neu
+		d0 = x_neu
+
+	print("Billiard fertig!") 
+	return (listX, listD)	
+
 """
 Parameter der Ellipse
 """
@@ -30,6 +54,9 @@ a = 2.0
 
 global b
 b = 1.0
+
+global eps
+eps = 0.0000001
 """
 In this example the center of the ellipse is always the origin
 """
@@ -110,6 +137,26 @@ def neuerPunkt(s, steigung):
 	return (x_neu, y_neu)
 
 """
+alt ist der alte Punkt, d ist Richtungsvektor
+"""
+def neuerPunktV(alt, d):
+	global a
+	global b
+	d = d * 1.0
+	
+	sol = solveMitternacht((d[0] / a)**2 + (d[1]/b)**2 , 2 * ( (d[0] * alt[0] / a**2 )+(d[1] * alt[1] / b**2 )) , (alt[0]/a)**2 + (alt[1]/b)**2 -1 )
+	
+	if (sol[0] != 0) : 
+		neu = alt + (sol[0] * d)
+	elif (sol[1] != 0) :
+		neu = alt + (sol[1] * d)
+	else :
+		raiseError("Es gibt nur eine Lösung!")
+	
+	'Normieren'
+	return neu / np.linalg.norm(neu)
+
+"""
 Löst die Mitternachtsformel
 """
 def solveMitternacht(a,b,c):
@@ -136,7 +183,9 @@ Berechne die normierte Normale der Gerade
 def berechneNormale(x):
 	global a
 	global b
-	normale = np.array( [x[0]/(a**2)] , [x[1]/(b**2)] )	
+	normale = np.array(( x[0]/(a**2) , x[1]/(b**2) ))
+
+	'Normieren'	
 	return normale / np.linalg.norm(normale)	
 
 
@@ -157,10 +206,10 @@ def berechneNeueSteigung(x, m_alt):
 	gerade_neu
 
 	'Drehsinn herausfinden'
-	if (drehMatrix2D(winkel) * gerade_alt == normale) :
-		gerade_neu = drehMatrix2D(2*winkel) * gerade_alt
-	elif (drehMatrix2D(-winkel) * gerade_alt == normale) : 
-		gerade_neu = drehMatrix2D(-2*winkel) * gerade_alt
+	if np.linalg.norm(np.dot(drehMatrix2D(winkel) , gerade_alt) - normale) < eps :
+		gerade_neu = np.dot(drehMatrix2D(2*winkel) , gerade_alt)
+	elif np.linalg.norm(np.dot(drehMatrix2D(-winkel) , gerade_alt) - normale) < eps : 
+		gerade_neu = np.dot(drehMatrix2D(-2*winkel),gerade_alt)
 	else :
 		raise Error('Alte Gerade konnte nicht auf Normale gedreht werden')
 	
@@ -168,18 +217,36 @@ def berechneNeueSteigung(x, m_alt):
 	if (gerade_neu[0] != 0) :	
 		gerade_neu = gerade_neu / gerade_neu[0]
 	else :
-		raise Errror('Neue Steigung ist unendlich')
+		raise Error('Neue Steigung ist unendlich')
 	
 	return gerade_neu[1]
-	 
+
+"""
+Berechnet die neue Richtung d_neu am Ellipsenpunkt x, sodass der Winkel zwischen d_alt und normale = Winkel zwischen d_neu und normale ist.
+"""
+def berechneNeueRichtung(x, d_alt) :
+	global eps
+	'Berechne Normale am Punkt x'
+	normale = berechneNormale(x)
+
+	'Normieren'
+	d_alt = d_alt / np.linalg.norm(d_alt)
+
+	cosphi = np.dot(d_alt, normale)
+	winkel = np.arccos( cosphi )
+
+	'Drehsinn herausfinden'
+	if np.linalg.norm(np.dot(drehMatrix2D(winkel) , d_alt) - normale) < eps :
+		d_neu = np.dot(drehMatrix2D(2*winkel), d_alt)
+	elif np.linalg.norm(np.dot(drehMatrix2D(-winkel) , d_alt) - normale) < eps : 
+		d_neu = np.dot(drehMatrix2D(-2*winkel), d_alt)
+	else :
+		raise Error("Drehung schlug fehl")
+	
+	return d_neu / np.linalg.norm(d_neu)
+
 
 def drehMatrix2D(winkel):
 	sin = np.sin(winkel)
 	cos = np.cos(winkel)
-	return np.array( ((cos, -sin) , (sin , cos)) )
-	
-
-
-	
-
-
+	return np.array(( (cos, -sin) , (sin , cos) ))
